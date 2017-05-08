80fc682f4cbc2c4bcadd8c87b6de47f6f0fa17e3
Dave Halter <davidhalter88@gmail.com>
2017-05-08 20:02:37 +0200

    Added the deploy script.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+# Create tag
+git tag $(python -c 'import parso; print(parso.__version__)')
+git push --tags
+
+# Package and upload to PyPI
+rm -rf dist/
+python setup.py sdist bdist_wheel
+# Maybe do a pip install twine before.
+twine upload dist/*
