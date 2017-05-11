150fb4c86e3f6a5665b39864ccce550c079200aa
Dave Halter <davidhalter88@gmail.com>
2017-05-11 07:13:23 -0400

    Add an initial version of the code from Jedi.
    Still includes imports that have to be removed.

---
+++ parso/__init__.py
@@ -0,0 +1,8 @@
+from parso.parser import ParserSyntaxError
+from parso.pgen2.pgen import generate_grammar
+from parso import python
+
+
+def parse(grammar, code):
+    raise NotImplementedError
+    Parser(grammar, code)
