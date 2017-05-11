944ceae0af01e8f79cb6ffd9442c0315356c61ff
Dave Halter <davidhalter88@gmail.com>
2017-05-11 14:41:21 -0400

    Add a version number.

--- parso/__init__.py
+++ parso/__init__.py
@@ -3,6 +3,9 @@ from parso.pgen2.pgen import generate_grammar
 from parso import python
 
 
+__version__ = '0.0.1'
+
+
 def parse(grammar, code):
     raise NotImplementedError
     Parser(grammar, code)
