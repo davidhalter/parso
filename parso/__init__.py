82868580a2a6e25bf376247f3a36adcdf733746a
Dave Halter <davidhalter88@gmail.com>
2017-09-04 21:38:57 +0200

    Fix a docstring.

--- parso/__init__.py
+++ parso/__init__.py
@@ -1,4 +1,4 @@
-"""
+r"""
 Parso is a Python parser that supports error recovery and round-trip parsing
 for different Python versions (in multiple Python versions). Parso is also able
 to list multiple syntax errors in your python file.
