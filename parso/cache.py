d5eb96309c37df24c75b016eb113929d0fa61585
Dave Halter <davidhalter88@gmail.com>
2019-04-03 01:07:25 +0200

    Increase the pickle version. With all the changes lately, it's better this way

--- parso/cache.py
+++ parso/cache.py
@@ -18,7 +18,7 @@ from parso._compatibility import FileNotFoundError
 LOG = logging.getLogger(__name__)
 
 
-_PICKLE_VERSION = 30
+_PICKLE_VERSION = 31
 """
 Version number (integer) for file system cache.
 
